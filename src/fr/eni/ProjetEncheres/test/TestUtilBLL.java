70a120b5de7a18c8a161c02680fac0295c602eae
Nolwenn <nolwenn.claquin2020@campus-eni.fr>
2021-01-15 09:06:04 +0100

    commit 15 janv 9h

--- src/fr/eni/ProjetEncheres/test/TestUtilBLL.java
+++ src/fr/eni/ProjetEncheres/test/TestUtilBLL.java
@@ -20,13 +20,22 @@ public class TestUtilBLL {
 //		um.updateUtilisateur(Julien);
 		//um.deleteUtilisateur(5);
 		//System.out.println(um.getListUtilisateur());
-		Utilisateur Julie= new Utilisateur("jul", "jolie", "julie", "ffglflyufv", "  ", "lkj", "29750", "loctudy", "password");
+		//Utilisateur Julie= new Utilisateur("jul", "jolie", "julie", "ffglflyufv", "  ", "lkj", "29750", "loctudy", "password");
 		
 		//um.addUtilisateur(Julie);
-		
+//		Utilisateur Capucine = new Utilisateur("la coquine", "Ric", "Capucine", "tyu", "022", "ljhl", "2147", "rennes", "doudou");
+//		
+//		um.addUtilisateur(Capucine);
 		
 		//System.out.println(um.getUtilisateurParPseudo("vinc"));
-		System.out.println(um.getUtilisateurParId(10));
+		//System.out.println(um.getUtilisateurParId(10));
+		//um.deleteUtilisateur(17);
+//		Utilisateur Herve = new Utilisateur("le grand .+blond", "Ric", "Herve", "tyu", "022", "ljhl", "2147", "rennes", "doudou");
+//		um.addUtilisateur(Herve);
+		
+		Utilisateur Herve3 = new Utilisateur("le grand $blond", "Rico", "RV", "tyu", "022", "ljhl", "2147", "rennes", "doudou");
+		um.addUtilisateur(Herve3);
+		//um.deleteUtilisateur(27);
 		
 	}
 
