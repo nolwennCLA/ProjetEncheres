d15ec8ea61e9aa3baa9a3e0aeeb8f73569ee1859
claire <claire.denis2020@campus-eni.fr>
2021-01-19 09:36:42 +0100

    Commit
    Commit

--- src/fr/eni/ProjetEncheres/ihm/enchere/DetailVenteServlet.java
+++ src/fr/eni/ProjetEncheres/ihm/enchere/DetailVenteServlet.java
@@ -8,9 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.eni.ProjetEncheres.bll.enchere.EnchereManager;
-import fr.eni.ProjetEncheres.bll.enchere.EnchereManagerSingl;
-
 /**
  * Servlet implementation class DetailVenteServlet
  */
@@ -31,7 +28,7 @@ public class DetailVenteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		EnchereManager em = EnchereManagerSingl.getInstance();
+//		EnchereManager em = EnchereManagerSingl.getInstance();
 		
 		
 		
